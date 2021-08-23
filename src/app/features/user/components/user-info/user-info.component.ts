c9b27c43f75447c3a25760c6a2f34683036122e3
Sabbir Ahmed <sabbirahmed@eatlbd.com>
2021-08-23 20:50:59 +0600

    Work on the file

--- src/app/features/user/components/user-info/user-info.component.ts
+++ src/app/features/user/components/user-info/user-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
 
 @Component({
   selector: 'app-user-info',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public webStorageService: WebStorageService
+  ) { }
 
   ngOnInit(): void {
   }
